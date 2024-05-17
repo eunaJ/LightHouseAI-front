9db911c794ddc1b90e1497e885e21440dcbef2f9
Euna Jeong <kysn75@gmail.com>
2024-05-17 23:41:23 +0900

    feature: 반복 위한 Travel 컴포넌트 구현

--- src/components/travel/Travel.js
+++ src/components/travel/Travel.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
-const Travel = ({ id, title, image_url}) => {
+const Travel = ({ id, title, writer, image_url, serving, expense, star}) => {
   return (
-    <div>
+    <div style={{padding: "3%"}}>
       <h2>{title}</h2>
-      <img src={image_url} alt='여행지 이미지'></img>
-      {/* <h5>{createdBy}</h5> */}
-      <hr />
-      {/* <p>{contents}</p> */}
+      <h5>{writer}</h5>
+      {image_url.includes(".png") && <img src={image_url} alt='여행지 이미지'></img>}
+      {serving && <p>{serving}명과 함께</p>}
+      {expense && <p>총 {expense}원</p>}
+      {star && <p>별점 {star}</p>}
     </div>
   );
 };
