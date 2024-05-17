4fe99f4175e1e89d56435fd2d14875f7103cda89
Euna Jeong <kysn75@gmail.com>
2024-05-17 23:50:10 +0900

    feature: Travel Card 불필요한 부분 삭제 및 디자인 수정

--- src/components/travel/TravelCard.js
+++ src/components/travel/TravelCard.js
@@ -1,28 +1,14 @@
 import "./TravelCard.css";
-import { useNavigate } from "react-router-dom";
 
-const TravelCard = ({ id, title, serving, star, imageUrl }) => {
-    // const getTravel = async () => {
-    //   const res = axios.get(`http://localhost:8080/api/v1/cafes/${id}`);
-    //   console.log(res);
-    // }
-
-    const navigate = useNavigate();
-
-    const getTravel = () => {
-        // navigate(`/travel/${id}`);
-    }
-
+const TravelCard = ({ id, title, writer, star, image_url }) => {
     return (
         <div className="travelcard-container" key={id}>
             <div className="travelcard">
-                <p>{id}</p>
-                <h5 className="travelcard-title">{title}</h5>
-                <div className="travelcard-body">
-                    {imageUrl && <img src={imageUrl} alt='travel_image' />}
-                    {/* <p>인원: {serving}</p> */}
-                    <p>별점: {star}</p>
-                    {/* <p className="travelcard-text">{description}</p> */}
+                {title ? (<h3 className="travelcard-title">{title}</h3>) : (<h3> </h3>)}
+                <div className="travelcard-body" style={{ color: "black" }}>
+                    {image_url.includes(".png") && <img src={image_url} alt='여행지 이미지'></img>}
+                    <p>{writer}</p>
+                    <p>{star}</p>
                 </div>
             </div>
         </div>
