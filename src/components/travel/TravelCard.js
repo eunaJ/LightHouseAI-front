15a2e421a53272b3166681477a57a789c6874786
Euna Jeong <kysn75@gmail.com>
2024-05-22 19:54:02 +0900

    refactor: 여행지 카드 디자인 수정

--- src/components/travel/TravelCard.js
+++ src/components/travel/TravelCard.js
@@ -5,28 +5,29 @@ const TravelCard = ({ id, title, writer, expense, star, image_url }) => {
     return (
         <div className="travelcard-container" key={id}>
             <div className="travelcard">
-                <div className="travelcard-img" style={{height:"50%"}}>
+                <div className="travelcard-img" style={{ height: "50%" }}>
                     {image_url && ['.png', '.jpeg', '.jpg'].some(ext => image_url.includes(ext)) ? (
-                        <img src={image_url} alt="여행지 이미지" style={{width: "100%", maxHeight:"100%"}} />
+                        <img src={image_url} alt="여행지 이미지" style={{ width: "100%", maxHeight: "100%" }} />
                     ) : (
-                        <img src={lighthouseaiLogo} alt="기본 이미지" style={{ width: "100%", maxHeight:"100%" }} />
+                        <img src={lighthouseaiLogo} alt="기본 이미지" style={{ width: "100%", maxHeight: "100%" }} />
                     )}
                 </div>
-                <hr style={{ color: "black", height: "0.1px", marginTop:"0.1px" }} />
+                <hr style={{ color: "black", height: "0.1px", marginTop: "0.1px" }} />
                 {title ? (<h3 className="travelcard-title">{title}</h3>) : (<h3> </h3>)}
-                <div className="travelcard-body" style={{ color: "black" }}>
-                    {expense !== 0 ? (
-                        <p style={{ marginTop: "2%" }}>총 {expense}원</p>
-                    ) : null}
+                <div style={{ display: "flex", alignItems: "center", marginTop: "2%" }}>
+                &nbsp;
                     {star !== 0 && (
-                        <p>
-                            별점{' '}
+                        <p style={{ margin: "0" }}>
                             {[...Array(star)].map((_, index) => (
                                 <span key={index}>★</span>
                             ))}
+                            {' '} {star} 
                         </p>
                     )}
+                    &nbsp;
+                    {expense !== 0 ? <p style={{ margin: "0 5px 0 0" }}>/ 총 {expense}원</p> : null}
                 </div>
+
             </div>
         </div>
     );
